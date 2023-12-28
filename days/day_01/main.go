307482a96fca4e7dc3ea57bbcb5c8ebbb65e0512
Raoul Kent <raoulkent@gmail.com>
2023-12-28 12:35:57 +0100

    day 1 part 2 solved

--- days/day_01/main.go
+++ days/day_01/main.go
@@ -3,6 +3,7 @@ package day_01
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,7 +18,7 @@ func Run(input []string, mode int) {
 }
 
 // Part1 solves the first part of the exercise
-func Part1(input []string) int {
+func Part1(input []string) string {
 	start := time.Now()
 	// Defer time tracking:
 	defer func() {
@@ -28,7 +29,22 @@ func Part1(input []string) int {
 	for _, line := range input {
 		sum += sumFirstAndLastNumber(line)
 	}
-	return sum
+	return strconv.Itoa(sum)
+}
+
+// Part2 solves the second part of the exercise
+func Part2(input []string) string {
+	start := time.Now()
+	// Defer time tracking:
+	defer func() {
+		fmt.Printf("Part 2 took: %v\n", time.Since(start))
+	}()
+
+	sum := 0
+	for _, line := range input {
+		sum += combineFirstAndLastDigitOrWord(line)
+	}
+	return strconv.Itoa(sum)
 }
 
 // Function that reads a string, and returns the first character that is a number from the left
@@ -59,7 +75,52 @@ func sumFirstAndLastNumber(line string) int {
 	return value
 }
 
-// Part2 solves the second part of the exercise
-func Part2(input []string) string {
+var wordToNumber = map[string]int{
+	"zero":  0,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+// Function that reads a string, and returns the first character or word that is a number from the left
+func getFirstNumberIncludingWords(line string) string {
+	for i := 0; i < len(line); i++ {
+		for word, number := range wordToNumber {
+			if strings.HasPrefix(line[i:], word) {
+				return strconv.Itoa(number)
+			}
+		}
+		if line[i] >= '0' && line[i] <= '9' {
+			return string(line[i])
+		}
+	}
 	return ""
+}
+
+// Function that reads a string, and returns the first character or word that is a number from the right
+func getLastNumberIncludingWords(line string) string {
+	for i := len(line) - 1; i >= 0; i-- {
+		for word, number := range wordToNumber {
+			if strings.HasSuffix(line[:i+1], word) {
+				return strconv.Itoa(number)
+			}
+		}
+		if line[i] >= '0' && line[i] <= '9' {
+			return string(line[i])
+		}
+	}
+	return ""
+}
+
+// Function that combines the first and last number or word of a string
+func combineFirstAndLastDigitOrWord(line string) int {
+	numbers := getFirstNumberIncludingWords(line) + getLastNumberIncludingWords(line)
+	value, _ := strconv.Atoi(numbers)
+	return value
 }
